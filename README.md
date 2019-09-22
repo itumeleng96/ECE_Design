5ab103a05257d5c70a78a780c9c3b562e9644c09
itumeleng96 <itukzz96@gmail.com>
2019-09-22 14:13:15 +0200

    Edited Readme File

--- README.md
+++ README.md
@@ -7,3 +7,27 @@ Mpooa Moeketsi
 Mapulane 
 Tlotlisang Lekena 
 
+__The following steps are followed to implement the Single Sonar Pinger__ 
+
+_Transmit chirp pulse in Julia_
+
+- chirp pulse generated according to the requirements 
+- sent over serial Port to the Teensy 
+- Recieved signal is transmitted with the DAC0 on pin A21 
+- Goes through amplifier circuit
+ 
+_Recieve the chirp Echoe on Teensy ADC pin A14_
+
+- A conversion character "write(sp,'c')" is sent from the Julia to start sampling
+- The recieved signal is printed on the serialPort with write(sp,"p")
+- The recieved signal is converted to values and to be processed
+
+
+_Process the recieved signal in Julia using FFT()_
+
+- Ensure that the recieved signal and transmitted are on the same axis 
+- Do FFT()
+- Show the output with PyPlot() and do axis adjustments 
+
+_Implement Proper Logic_ to allow interaction with the User
+
--- STBlock.jl
+++ STBlock.jl
@@ -1,3 +1,5 @@
+using SerialPorts
+using PyPlot
 
 sp = SerialPort(list_serialports()[1], 9600)  # port of the Teensy if connected
 
@@ -14,7 +16,53 @@ t = collect(0:dt:t_max_pulse);
 rect(t)=(abs.(t) .<=0.5)*1.0;
 v_tx = UInt8.(round.((cos.(2*pi*(f*(t.-t_d).+0.5*K*(t.-t_d).^2)).*rect((t .-t_d)/T).+1).*127));
 
-#plot(t,v_tx)
+#clear the serial buffer
+s=readavailable(sp)  #clear the serial buffer
+
+write(sp,'c')	     #start conversion 
+while bytesavailable(sp)<1
+	continue
+	sleep(0.05)
+end
+
 s = readavailable(sp)
-write(sp,'c')	 	         	            # write an array of Uint8 to the port
-s = readavailable(sp) 		                # read ADC string values from the port
+print(s)	    		#print the time for conversion
+
+#Get the Values 
+
+write(sp,'p')
+while bytesavailable(sp)<1
+	continue        
+end 
+a=""
+i=0
+while true
+	global i
+	global a
+	x=readavailable(sp)
+	len=length(x)
+	println(len)
+	if len<1
+		sleep(0.005)
+		if len<1
+			break
+		end
+	end
+	b=x
+	a=string(a,b)
+	i+=1
+	println("a",length(a))
+end
+
+adc=split(a,"\r\n")
+
+len=length(adc)
+v=Vector{Int64}(undef,len)
+
+for i=1:len
+           global v
+           global adc
+           v[i]=parse(Int64,adc[i])
+       end
+plot(v)
+
